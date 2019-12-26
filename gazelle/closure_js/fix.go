8608fb89aed5cc1a3e7e000089609f1f654d89d1
Rob Figueiredo <robfig@yext.com>
2019-12-26 13:41:00 -0500

    gazelle module for automatically composing closure_js_* BUILD rules
    This change uses Gazelle (https://github.com/bazelbuild/bazel-gazelle),
    a BUILD file generator for Bazel projects, to generate rules_closure BUILD
    files automatically based on the directory structure of a preexisting
    project.
    
    It composes library, binary and test rules for JS, and it also recognizes JSX
    files and composes similar rules for those. Since rules_closure does not provide
    any JSX processing rules, the user must bring their own via the map_kind
    Gazelle directive.
    
    This is part of the rules_closure repo since the BUILD rules generated should be
    coordinated with the BUILD rules themselves, which can avoid a compatibility
    matrix.
    
    We have been using and improving this at work for about 6 months.

---
+++ gazelle/closure_js/fix.go
@@ -0,0 +1,9 @@
+package closure_js
+
+import (
+	"github.com/bazelbuild/bazel-gazelle/config"
+	"github.com/bazelbuild/bazel-gazelle/rule"
+)
+
+func (_ *jsLang) Fix(c *config.Config, f *rule.File) {
+}
