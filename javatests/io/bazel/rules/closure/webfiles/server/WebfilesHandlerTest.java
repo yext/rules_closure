93e049a3895ac512a14900956cb3a816efce8a82
Goktug Gokdogan <goktug@google.com>
2020-02-03 15:01:50 -0800

    Migrate static imports to org.mockito.ArgumentMatchers (#467)
    .. argument matcher methods from Matchers are deprecated in favor of ArgumentMatchers.

--- javatests/io/bazel/rules/closure/webfiles/server/WebfilesHandlerTest.java
+++ javatests/io/bazel/rules/closure/webfiles/server/WebfilesHandlerTest.java
@@ -15,7 +15,7 @@
 package io.bazel.rules.closure.webfiles.server;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
