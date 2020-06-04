5ee2dc792a7ed541aca610731d2b4f5be4ef6414
Rob Figueiredo <robfig@yext.com>
2020-06-04 10:29:26 -0400

    gazelle: implement rule per directory for tests
    Tests are more complicated because they may have an associated HTML file.

--- gazelle/closure_js/integration_test.go
+++ gazelle/closure_js/integration_test.go
@@ -157,17 +157,17 @@ closure_js_library(
 		{
 			Path: "map_kind/BUILD.bazel",
 			Content: `
-load("//tools/js:defs.bzl", "closure_js_externs")
+load("//tools/js:defs.bzl", "closure_js_extern_library")
 
-# gazelle:map_kind closure_js_library closure_js_externs //tools/js:defs.bzl
+# gazelle:map_kind closure_js_library closure_js_extern_library //tools/js:defs.bzl
 
-closure_js_externs(
+closure_js_extern_library(
     name = "existing",
     srcs = ["file1.js"],
     visibility = ["//visibility:public"],
 )
 
-closure_js_externs(
+closure_js_extern_library(
     name = "deleted",
     srcs = ["deleted.js"],
     visibility = ["//visibility:public"],
@@ -382,17 +382,17 @@ closure_js_library(
 		{
 			Path: "map_kind/BUILD.bazel",
 			Content: `
-load("//tools/js:defs.bzl", "closure_js_externs")
+load("//tools/js:defs.bzl", "closure_js_extern_library")
 
-# gazelle:map_kind closure_js_library closure_js_externs //tools/js:defs.bzl
+# gazelle:map_kind closure_js_library closure_js_extern_library //tools/js:defs.bzl
 
-closure_js_externs(
+closure_js_extern_library(
     name = "existing",
     srcs = ["file1.js"],
     visibility = ["//visibility:public"],
 )
 
-closure_js_externs(
+closure_js_extern_library(
     name = "file2",
     srcs = ["file2.js"],
     visibility = ["//visibility:public"],
@@ -696,6 +696,7 @@ const file1 = goog.require('corp.existing.file1');
 		},
 		{Path: "existing_rules/file5.js"},
 		{Path: "existing_rules/file6.js"},
+		{Path: "existing_rules/testdata"},
 		{
 			Path: "existing_rules/BUILD.bazel",
 			Content: `
@@ -705,6 +706,10 @@ closure_js_library(
     name = "existing",
     srcs = ["file1.js", "file2.js"],
     visibility = ["//visibility:public"],
+    data = ["testdata"],
+    deps = [
+        ":testdep", # keep
+    ]
 )
 
 closure_js_library(
@@ -947,8 +952,10 @@ closure_js_library(
         "file1.js",
         "file2.js",
     ],
+    data = ["testdata"],
     visibility = ["//visibility:public"],
     deps = [
+        ":testdep",  # keep
         "@io_bazel_rules_closure//closure/library/string",
         "@io_bazel_rules_closure//third_party/closure/library/dojo/dom:query",
     ],
@@ -1120,6 +1127,566 @@ closure_js_library(
     srcs = ["file2.js"],
     visibility = ["//visibility:public"],
 )
+`,
+		},
+	})
+}
+
+func TestJSTestRulePerDirectory(t *testing.T) {
+	files := []testtools.FileSpec{
+		{Path: "WORKSPACE"},
+
+		// Test that a package with no rules has files combined into a directory.
+		{
+			Path:    "no_rules/norules.js",
+			Content: `goog.provide('no_rules')`,
+		},
+		{
+			Path:    "no_rules/norules_test.js",
+			Content: ``,
+		},
+
+		// Test that default visibility is respected.
+		{
+			Path:    "default_vis/BUILD.bazel",
+			Content: `package(default_visibility = ["//a:__subpackages__"])`,
+		}, {
+			Path: "default_vis/defaultvis_test.js",
+		},
+
+		// Test that a package with multiple tests has them combined.
+		{Path: "no_rules/multiple_files/a_test.js"},
+		{Path: "no_rules/multiple_files/b_test.js"},
+		{Path: "no_rules/multiple_files/c_test.js"},
+
+		// Test that a rule that includes a jsx file uses closure_jsx_library.
+		{Path: "no_rules/multiple_files_jsx/a_test.js"},
+		{Path: "no_rules/multiple_files_jsx/b_test.jsx"},
+		{Path: "no_rules/multiple_files_jsx/c_test.js"},
+
+		// Test that a rule that includes a HTML file is broken out.
+		{Path: "no_rules/multiple_files_html/a_test.js"},
+		{Path: "no_rules/multiple_files_html/b_test.jsx"},
+		{Path: "no_rules/multiple_files_html/b_test.html"},
+		{Path: "no_rules/multiple_files_html/c_test.js"},
+
+		// Test that a package with existing rules (but no directory-level rule)
+		// maintains the existing ones and adds new files to a library-wide
+		// rule.
+		{Path: "existing_rules/file1.js", Content: "goog.provide('existing_rules.file1');"},
+		{Path: "existing_rules/file2.js", Content: "goog.provide('existing_rules.file2');"},
+		{Path: "existing_rules/file1_test.js",
+			Content: "goog.require('goog.string');\ngoog.require('existing_rules.file1');"},
+		{Path: "existing_rules/file2_test.js",
+			Content: "goog.require('goog.dom.query');\ngoog.require('existing_rules.file2');"},
+		{Path: "existing_rules/file3_test.js"},
+		{Path: "existing_rules/file4_test.jsx"},
+		{Path: "existing_rules/file5_test.js"},
+		{
+			Path: "existing_rules/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_library(
+    name = "existing_rules",
+    srcs = ["file1.js"]
+)
+
+closure_js_library(
+    name = "file2",
+    srcs = ["file2.js"],
+)
+
+closure_js_test(
+    name = "existing",
+    srcs = ["file1_test.js", "file2_test.js"],
+)
+
+closure_js_test(
+    name = "deleted",
+    srcs = ["deleted.js"],
+)
+
+closure_js_test(
+    name = "deleted_multifile",
+    srcs = ["deleted1.js", "deleted2.js"],
+)
+
+closure_js_test(
+    name = "deleted_some_multifile",
+    srcs = ["file3_test.js", "deleted3.js"],
+)
+`,
+		},
+
+		// Test that a package with an existing directory-level rule adds to it.
+		// Test that dependencies of individual files are combined.
+		{
+			Path: "existing_lib/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_library(
+    name = "existing_lib",
+    srcs = ["file1.js"],
+)
+
+closure_js_test(
+    name = "existing_lib_test",
+    srcs = ["file1_test.js"],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+		{Path: "existing_lib/file1.js", Content: "goog.provide('existing_lib.file1');"},
+		{Path: "existing_lib/file1_test.js", Content: "goog.require('goog.array');"},
+		{Path: "existing_lib/file2_test.js",
+			Content: "goog.provide('existing_lib.file2_test');\ngoog.require('existing_lib.file1');"},
+
+		// Test changing type of a rule as a jsx file is added.
+		{
+			Path: "change_type/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_test(
+    name = "change_type_test",
+    srcs = ["file1_test.js"],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+		{Path: "change_type/file1_test.js"},
+		{Path: "change_type/file2_test.jsx"},
+
+		// Test moving a test out of the directory-level rule if it gains a HTML.
+		{
+			Path: "gains_html/BUILD.bazel",
+			Content: `
+		load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+		closure_js_test(
+		    name = "gains_html_test",
+		    srcs = ["a_test.js", "b_test.js"],
+		    visibility = ["//visibility:public"],
+		)
+		`,
+		},
+		{Path: "gains_html/a_test.js"},
+		{Path: "gains_html/b_test.js"},
+		{Path: "gains_html/b_test.html"},
+
+		// Test how a file with same name as directory and HTML is handled.
+		//
+		// Normally it would be broken out as a single-file rule, but the name
+		// would conflict.
+		{Path: "dir_has_html/dir_has_html_test.js", Content: "goog.require('goog.string');"},
+		{Path: "dir_has_html/dir_has_html_test.html"},
+
+		// Test handling a file gaining HTML when named the same as the directory.
+		{
+			Path: "gains_html_2/BUILD.bazel",
+			Content: `
+		load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+		closure_js_test(
+		    name = "gains_html_2_test",
+		    srcs = ["gains_html_2_test.js", "b_test.js"],
+		    visibility = ["//visibility:public"],
+		)
+		`,
+		},
+		{Path: "gains_html_2/gains_html_2_test.js", Content: `goog.require('goog.array');`},
+		{Path: "gains_html_2/gains_html_2_test.html"},
+		{Path: "gains_html_2/b_test.js", Content: `goog.require('goog.string');`},
+
+		// Test a single rule whose source uses a different type.
+		{
+			Path: "change_type_2/BUILD.bazel",
+			Content: `
+        # gazelle:map_kind closure_jsx_test closure_jsx_test //tools/js:defs.bzl
+
+		closure_js_test(
+		    name = "a_test",
+		    srcs = ["a_test.jsx"],
+            data = ["testdata"],
+		)
+
+		closure_jsx_test(
+		    name = "b_test",
+		    srcs = ["b_test.js"],
+            data = ["testdata"],
+		)
+
+		closure_jsx_test(
+		    name = "c_test",
+		    srcs = ["c_test.js"],
+            data = ["testdata"],
+		)
+
+		closure_js_test(
+		    name = "d_test",
+		    srcs = ["d_test.jsx"],
+            data = ["testdata"],
+		)
+`,
+		},
+		{Path: "change_type_2/a_test.jsx"},
+		{Path: "change_type_2/a_test.html"},
+		{Path: "change_type_2/b_test.js"},
+		{Path: "change_type_2/b_test.html"},
+		{Path: "change_type_2/c_test.js"},
+		{Path: "change_type_2/d_test.jsx"},
+		{Path: "change_type_2/testdata"},
+	}
+
+	dir, cleanup := testtools.CreateFiles(t, files)
+	defer cleanup()
+
+	if err := runGazelle(t, dir, []string{}); err != nil {
+		t.Fatal(err)
+	}
+
+	testtools.CheckFiles(t, dir, []testtools.FileSpec{
+		{
+			Path: "no_rules/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_library", "closure_js_test")
+
+closure_js_library(
+    name = "no_rules",
+    srcs = ["norules.js"],
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "no_rules_test",
+    srcs = ["norules_test.js"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/no_rules/norules_test"],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "default_vis/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+package(default_visibility = ["//a:__subpackages__"])
+
+closure_js_test(
+    name = "default_vis_test",
+    srcs = ["defaultvis_test.js"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/default_vis/defaultvis_test"],
+)
+`,
+		},
+
+		{
+			Path: "no_rules/multiple_files/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_test(
+    name = "multiple_files_test",
+    srcs = [
+        "a_test.js",
+        "b_test.js",
+        "c_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/no_rules/multiple_files/a_test",
+        "/no_rules/multiple_files/b_test",
+        "/no_rules/multiple_files/c_test",
+    ],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "no_rules/multiple_files_jsx/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_jsx_test")
+
+closure_jsx_test(
+    name = "multiple_files_jsx_test",
+    srcs = [
+        "a_test.js",
+        "b_test.jsx",
+        "c_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/no_rules/multiple_files_jsx/a_test",
+        "/no_rules/multiple_files_jsx/b_test",
+        "/no_rules/multiple_files_jsx/c_test",
+    ],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "no_rules/multiple_files_html/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test", "closure_jsx_test")
+
+closure_jsx_test(
+    name = "b_test",
+    srcs = ["b_test.jsx"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/no_rules/multiple_files_html/b_test"],
+    html = "b_test.html",
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "multiple_files_html_test",
+    srcs = [
+        "a_test.js",
+        "c_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/no_rules/multiple_files_html/a_test",
+        "/no_rules/multiple_files_html/c_test",
+    ],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "existing_rules/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_library", "closure_js_test", "closure_jsx_test")
+
+closure_js_library(
+    name = "existing_rules",
+    srcs = ["file1.js"],
+    visibility = ["//visibility:public"],
+)
+
+closure_js_library(
+    name = "file2",
+    srcs = ["file2.js"],
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "existing",
+    srcs = [
+        "file1_test.js",
+        "file2_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/existing_rules/file1_test",
+        "/existing_rules/file2_test",
+    ],
+    visibility = ["//visibility:public"],
+    deps = [
+        ":existing_rules",
+        ":file2",
+        "@io_bazel_rules_closure//closure/library/string",
+        "@io_bazel_rules_closure//third_party/closure/library/dojo/dom:query",
+    ],
+)
+
+closure_js_test(
+    name = "deleted_some_multifile",
+    srcs = ["file3_test.js"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/existing_rules/file3_test"],
+    visibility = ["//visibility:public"],
+)
+
+closure_jsx_test(
+    name = "existing_rules_test",
+    srcs = [
+        "file4_test.jsx",
+        "file5_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/existing_rules/file4_test",
+        "/existing_rules/file5_test",
+    ],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "existing_lib/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_library", "closure_js_test")
+
+closure_js_library(
+    name = "existing_lib",
+    srcs = ["file1.js"],
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "existing_lib_test",
+    srcs = [
+        "file1_test.js",
+        "file2_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/existing_lib/file1_test",
+        "existing_lib.file2_test",
+    ],
+    visibility = ["//visibility:public"],
+    deps = [
+        ":existing_lib",
+        "@io_bazel_rules_closure//closure/library/array",
+    ],
+)
+`,
+		},
+
+		{
+			Path: "change_type/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_jsx_test")
+
+closure_jsx_test(
+    name = "change_type_test",
+    srcs = [
+        "file1_test.js",
+        "file2_test.jsx",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/change_type/file1_test",
+        "/change_type/file2_test",
+    ],
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "gains_html/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_test(
+    name = "gains_html_test",
+    srcs = ["a_test.js"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/gains_html/a_test"],
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "b_test",
+    srcs = ["b_test.js"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/gains_html/b_test"],
+    html = "b_test.html",
+    visibility = ["//visibility:public"],
+)
+`,
+		},
+
+		{
+			Path: "dir_has_html/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_test(
+    name = "dir_has_html_test",
+    srcs = ["dir_has_html_test.js"],
+    compilation_level = "ADVANCED",
+    entry_points = ["/dir_has_html/dir_has_html_test"],
+    html = "dir_has_html_test.html",
+    visibility = ["//visibility:public"],
+    deps = ["@io_bazel_rules_closure//closure/library/string"],
+)
+`,
+		},
+
+		{
+			Path: "gains_html_2/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+
+closure_js_test(
+    name = "gains_html_2_test",
+    srcs = [
+        "b_test.js",
+        "gains_html_2_test.js",
+    ],
+    compilation_level = "ADVANCED",
+    entry_points = [
+        "/gains_html_2/b_test",
+        "/gains_html_2/gains_html_2_test",
+    ],
+    html = "gains_html_2_test.html",
+    visibility = ["//visibility:public"],
+    deps = [
+        "@io_bazel_rules_closure//closure/library/array",
+        "@io_bazel_rules_closure//closure/library/string",
+    ],
+)
+`,
+		},
+
+		{
+			Path: "change_type_2/BUILD.bazel",
+			Content: `
+load("@io_bazel_rules_closure//closure:defs.bzl", "closure_js_test")
+load("//tools/js:defs.bzl", "closure_jsx_test")
+
+# gazelle:map_kind closure_jsx_test closure_jsx_test //tools/js:defs.bzl
+
+closure_jsx_test(
+    name = "a_test",
+    srcs = ["a_test.jsx"],
+    compilation_level = "ADVANCED",
+    data = ["testdata"],
+    entry_points = ["/change_type_2/a_test"],
+    html = "a_test.html",
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "b_test",
+    srcs = ["b_test.js"],
+    compilation_level = "ADVANCED",
+    data = ["testdata"],
+    entry_points = ["/change_type_2/b_test"],
+    html = "b_test.html",
+    visibility = ["//visibility:public"],
+)
+
+closure_js_test(
+    name = "c_test",
+    srcs = ["c_test.js"],
+    compilation_level = "ADVANCED",
+    data = ["testdata"],
+    entry_points = ["/change_type_2/c_test"],
+    visibility = ["//visibility:public"],
+)
+
+closure_jsx_test(
+    name = "d_test",
+    srcs = ["d_test.jsx"],
+    compilation_level = "ADVANCED",
+    data = ["testdata"],
+    entry_points = ["/change_type_2/d_test"],
+    visibility = ["//visibility:public"],
+)
+
 `,
 		},
 	})
